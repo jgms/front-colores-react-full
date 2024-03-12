ef7eca623eba25ff4d53ed7073a1b7c2b0e61ef5
Joaquín <joaquinmendozasebastian@gmail.com>
2024-03-12 14:02:24 +0100

    commit inicial

---
+++ src/Colores.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+import Formulario from './Formulario'
+import Color from './Color'
+
+function Colores() {
+
+  let [colores,setColores] = useState([])
+
+  useEffect(() => {
+    fetch("https://api-colores-full-no3c.onrender.com/colores")
+    .then( respuesta => respuesta.json())
+    .then(respuesta => {
+      setColores(respuesta)
+    })
+  }, [])
+
+  function crearColor(color){
+    setColores([...colores,color])
+  }
+
+
+  function borrarColor(id){
+    fetch("https://api-colores-full-no3c.onrender.com/colores/borrar/" + id,{
+      method : "DELETE"
+    })
+    .then(respuesta => respuesta.json())
+    .then(({resultado}) => {
+      if(resultado == "ok"){
+        return setColores(colores.filter(color => color.id != id))
+      }
+      console.log("...error usuario")
+    })
+  }
+  
+
+  return (
+    <>
+      <Formulario crearColor={crearColor} />
+      <ul>
+        { colores.map (({id,r,g,b}) => <Color key={id} id={id} r={r} g={g} b={b} borrarColor={borrarColor} />) }
+      </ul>
+    </>
+  )
+}
+
+export default Colores
